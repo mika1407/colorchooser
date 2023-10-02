002c204a82408aaae652004e95a19505c14615e8
mika tiihonen <koneautomaatio@hotmail.com>
2023-10-02 21:56:41 +0300

    save file added

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import colorchooser
+from tkinter import filedialog
 
 def click():
   # color = colorchooser.askcolor()
@@ -13,6 +14,21 @@ def submit():
     input = text.get("1.0",END)
     print(input)
 
+def saveFile():
+   file = filedialog.asksaveasfile(initialdir="C:\\Users\\konea\\Documents\\Python\\Test",
+                                   defaultextension='.txt',
+                                   filetypes=[
+                                      ("Text file",".txt"),
+                                      ("HTML file",".html"),
+                                      ("All files", ".*"),
+                                   ])
+   if file is None:
+      return
+   filetext = str(text.get(1.0,END))
+   #filetext = input("Enter some text please: ")
+   file.write(filetext)
+   file.close()
+
 window = Tk()
 window.geometry("420x420")
 button = Button(text="change color",command=click)
@@ -21,13 +37,16 @@ button.pack()
 text = Text(window,
             bg="light yellow",
             font=("Ink Free",14),
-            height=14,
+            height=12,
             width=32,
             padx=10,
             pady=10,
             fg="purple")
 text.pack()
 button = Button(window,text="submit",command=submit)
+button.pack()
+
+button = Button(text='save',command=saveFile)
 button.pack()
 
 window.mainloop()
